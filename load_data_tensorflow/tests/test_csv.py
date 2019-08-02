2b1c8a91d6d1eac33ae1a2cbef60f45c2e09510e
akaashhazarika <akaashhazarika@gmail.com>
2019-08-02 16:32:04 -0400

    Update test_csv.py

--- load_data_tensorflow/tests/test_csv.py
+++ load_data_tensorflow/tests/test_csv.py
@@ -4,7 +4,7 @@ from load_data_tensorflow import csv
 
 class CSV(TestCase):
     def test_is_string(self):
-        test_result = [[[['mangoes', 4,], ['Python codes', 6]]], ['Keyword', 'Impressions', 'Cost_Micros']]
+        test_result = [[[['mangoes', 4,], ['Python codes', 6]]], ['Keyword','Cost_Micros']]
         column_names = test_result[1]
 
         obj = csv.csv_creator(test_result)
